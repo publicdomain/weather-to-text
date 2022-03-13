09e1313b70f924d9c39ca5fb82c545e63070a80f
Victor <publicdomainvic@gmail.com>
2022-03-13 08:44:01 -0400

    Update AssemblyInfo

--- WeatherToText/Properties/AssemblyInfo.cs
+++ WeatherToText/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="AssemblyInfo.cs" company="PublicDomainWeekly.com">
+﻿// <copyright file="AssemblyInfo.cs" company="PublicDomain.is">
 //     CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication
 //     https://creativecommons.org/publicdomain/zero/1.0/legalcode
 // </copyright>
@@ -13,17 +13,17 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("WeatherToText")]
 [assembly: AssemblyDescription("Displays the weather in the command line (weather forecast to text).")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("PublicDomainWeekly.com")]
+[assembly: AssemblyCompany("PublicDomain.is")]
 [assembly: AssemblyProduct("WeatherToText")]
-[assembly: AssemblyCopyright("Copyright 2021 - Victor L. Senior")]
-[assembly: AssemblyTrademark("publicdomainweekly")]
+[assembly: AssemblyCopyright("Copyright 2022 - Victor L. Senior")]
+[assembly: AssemblyTrademark("publicdomain")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.4.0.*")]
+[assembly: AssemblyVersion("0.5.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
